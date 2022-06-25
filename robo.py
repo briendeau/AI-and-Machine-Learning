b558468c35e4f73cb698d9cf43120547e382610a
briendeau <yamori425@gmail.com>
2022-06-25 02:10:16 -0400

    GUI automation -square-

---
+++ robo.py
@@ -0,0 +1,11 @@
+import pyautogui
+wh = pyautogui.size()
+print(wh.width)
+
+for i in range(10):
+    pyautogui.moveTo(100, 100, duration=0.25)
+    pyautogui.moveTo(200, 100, duration=0.25)
+    pyautogui.moveTo(200, 200, duration=0.25)
+    pyautogui.moveTo(100, 200, duration=0.25)
+
+    
