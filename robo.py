e743d49404081dbb53418bc5c9e0591d01659dc6
briendeau <yamori425@gmail.com>
2022-07-04 23:44:28 -0400

    comments

--- robo.py
+++ robo.py
@@ -2,10 +2,13 @@ import pyautogui
 wh = pyautogui.size()
 print(wh.width)
 
+
+#moving the mouse and drawing a square 10 times.
 for i in range(10):
-    pyautogui.moveTo(100, 100, duration=0.25)
-    pyautogui.moveTo(200, 100, duration=0.25)
-    pyautogui.moveTo(200, 200, duration=0.25)
-    pyautogui.moveTo(100, 200, duration=0.25)
+    pyautogui.move(100, 0, duration=0.25)
+    pyautogui.move(0, 100, duration=0.25)
+    pyautogui.move(-100, 0, duration=0.25)
+    pyautogui.move(0, -100, duration=0.25)
+    
 
     
